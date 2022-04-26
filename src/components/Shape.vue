383cfde410f9d4a573c3e4f6f248f97d79b3bf89
Ricardo Atakiama <atakiama@usp.br>
2022-04-25 21:44:25 -0300

    Iniciando estudos com Vue usando data properties em um componente

---
+++ src/components/Shape.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p> Welcome </p>
+
+    {{ data }}
+    {{ created }}
+    {{ mounted }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ShapeComponent',
+    data() {
+      return {
+        data: 'Ricardo',
+        created: 'Created',
+        mounted: 'Mounted'
+      }
+    }
+  }
+</script>
