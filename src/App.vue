819066f9f6170dd09ffd02bd132fe0cd75368913
Ricardo Atakiama <atakiama@usp.br>
2022-05-03 17:28:37 -0300

    Inserindo footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
- <CustomNavbar />
- <!-- router-view -->
+ <CustomNavbar :logo="logoSrc" :alt="appName" />
  <router-view />
+ <CustomFooter />
 </template>
 
 <script>
   import CustomNavbar from './components/CustomNavbar.vue'
+  import CustomFooter from './components/CustomFooter.vue'
 
   export default {
     name: 'My-app',
     components: {
-      CustomNavbar
-    } 
+      CustomNavbar,
+      CustomFooter
+    },
+    data() {
+      return {
+        logoSrc: './img/logo.png',
+        appName: 'Make your Burguer'
+      }
+    }
   }
 </script>
+
+<style>
+  * {
+    font-family: 'Proxima Nova';
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+</style>
+ 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// pode importar css file aqui // import './assets/new.css'
 
 createApp(App)
   .use(router)
